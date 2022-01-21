70f39148db34e085fed53bea0563854e7d7d3306
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-01-20 21:04:18 -0300

    Criando projeto

---
+++ src/br/com/adson/util/StandardException.java
@@ -0,0 +1,22 @@
+package br.com.adson.util;
+
+
+class StandardException extends Exception {
+    
+    public StandardException() {
+    
+    }
+    
+    public StandardException(String arg0) {
+        super(arg0);
+    }
+    
+    public StandardException(Throwable arg0) {
+        super(arg0);
+    }
+    
+    public StandardException(String arg0, Throwable arg1) {
+        super(arg0,arg1);
+    }
+    
+}
---
+++ src/br/com/adson/util/TesteJoptionP.java
@@ -0,0 +1,28 @@
+
+package br.com.adson.util;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author adson
+ */
+public class TesteJoptionP {
+    
+    public static void main(String[] args) {
+        
+    /*    String nome = JOptionPane.showInputDialog("Digite o nome: ");
+        String sobreNome = JOptionPane.showInputDialog("Digite o sobrenome: ");
+        
+        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: "));
+        
+        
+        String nomeCompleto = nome + " " + sobreNome;
+        
+        JOptionPane.showMessageDialog(null, "Nome Completo: " + nomeCompleto +"\n Idade: " + idade,
+                "Informação", JOptionPane.INFORMATION_MESSAGE);*/
+    
+    /* ConnectionFactory.getConexao(); */
+    }
+    
+}
