a7afa202fb09c7d9b6d149feded139f9b8b56d64
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-01-20 21:16:46 -0300

    Criando Class Dao

---
+++ src/br/com/adson/dao/Dao.java
@@ -0,0 +1,19 @@
+package br.com.adson.dao;
+
+import br.com.adson.util.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public abstract class Dao<Entity> {
+    
+    protected Connection obterConexao()throws SQLException {
+        return ConnectionFactory.getConexao();
+    }
+    
+    public abstract boolean cadastrar(Entity pojo) throws SQLException;
+    public abstract boolean atualizar(Entity pojo) throws SQLException;
+    public abstract boolean excluir(Entity pojo) throws SQLException;
+    public abstract Integer getCodigo(Entity pojo) throws SQLException;
+    
+}
